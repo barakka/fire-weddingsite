05b210acbc227d0ead250b963244fa26092c295a
barakka <github@barakka.org>
2014-04-12 23:01:10 +0000

    Added prod configuration

--- js/config.js
+++ js/config.js
@@ -1,4 +1,10 @@
-var fBaseRef = new Firebase("https://bodaaxelysol-dev.firebaseio.com");
+/* global Firebase */
+
+var devenv = (window.location.hostname.indexOf('-dev')>=0) ||
+	(window.location.hostname.indexOf('localhost')>=0);  
+
+var firebaseLocation = devenv ? "https://bodaaxelysol-dev.firebaseio.com" : "https://axelysolboda.firebaseio.com"; 
+var fBaseRef = new Firebase(firebaseLocation);
 var groupsRef = fBaseRef.child("groups");
 var groupsIndexRef = fBaseRef.child("groupsIndex");
 var usersRef = fBaseRef.child("users");
