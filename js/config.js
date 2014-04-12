449100d8ee3780721e55bc27d158436cf345f38a
barakka <github@barakka.org>
2014-04-12 22:37:40 +0000

    Completed move to firebase. Removed bower.

---
+++ js/config.js
@@ -0,0 +1,7 @@
+var fBaseRef = new Firebase("https://bodaaxelysol-dev.firebaseio.com");
+var groupsRef = fBaseRef.child("groups");
+var groupsIndexRef = fBaseRef.child("groupsIndex");
+var usersRef = fBaseRef.child("users");
+var profilesRef = fBaseRef.child("profiles");
+var mailingListRef = fBaseRef.child("mailingList");
+var commentsRef = fBaseRef.child("comments");
