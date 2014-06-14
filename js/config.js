34f0f3682454b51305e472f081d9ed67b289e60f
barakka <github@barakka.org>
2014-06-14 11:13:04 +0000

    Added analytics. Changed us photo. Looaded all css in index.

--- js/config.js
+++ js/config.js
@@ -12,3 +12,16 @@ var profilesRef = fBaseRef.child("profiles");
 var mailingListRef = fBaseRef.child("mailingList");
 var commentsRef = fBaseRef.child("comments");
 var photosRef = fBaseRef.child("photos");
+
+
+if (!devenv){
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-51910441-1', 'axelysolboda.appspot.com');
+  ga('send', 'pageview');
+}
+
+
